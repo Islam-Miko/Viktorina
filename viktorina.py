1166686cb415356a8ec57f79f819aa4e92868fb1
Aldaiar <aldaiardushebaev@gmail.com>
2022-01-28 21:13:19 +0600

    check answer

--- viktorina.py
+++ viktorina.py
@@ -2,13 +2,23 @@ class Question:
     def __init__(self, text, *ans):
         self.text = text
         self.answers = [an for an in ans]
-        self.choice= 1
+        self.choice = 1
+
 
 class Answers:
     def __init__(self, text: str, key: bool):
         self.text = text
         self.key = key
         self.number = 99
+
+
+class Result:
+    def __init__(self, number, time, amount):
+        self.number = number
+        self.time = time
+        self.amount = amount
+
+
 questions = [
     Question('1+1=?',
              Answers('3', False),
@@ -36,5 +46,45 @@ questions = [
              ),
 
 ]
+
+
+class Question:
+    def __init__(self, text, *answers):
+        self.text = text
+        self.answers = [ans for ans in answers]
+
+
+class Answer:
+    def __init__(self, text: str, key: bool):
+        self.text = text
+        self.key = key
+
+
+questions = [
+    Question(
+        '1+1=?',
+        Answer('1', False),
+        Answer('7', False),
+        Answer('3', False),
+        Answer('2', True)
+    )
+]
+
+for question in questions:
+    print(question.text)
+    for answer in question.answers:
+        print(answer.text)
+    choice = int(input('enter your choice:\n'))
+    if choice <= 0:
+        print('enter valid variant!')
+        break
+    try:
+        choice -= 1
+        if question.answers[choice].key:
+            print('right <3')
+        else:
+            print('wrong :(')
+    except IndexError:
+        print('enter valid variant!')
 
 
